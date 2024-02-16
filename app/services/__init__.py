10f1215b398443c20dfa9e56ff28eddd0a017b50
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2024-02-16 16:29:20 +0000

    wip

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,16 +1,12 @@
 import logging
 import sys
-import http
 
 from loguru import logger as _logger
 from settings import Settings
 
 settings = Settings()
 
-
 _LOGGING_LEVEL = logging.DEBUG if settings.DEBUG else logging.INFO
-
-
 logging.basicConfig(level=_LOGGING_LEVEL)
 logger = _logger
 logger.configure(handlers=[{"sink": sys.stderr, "level": _LOGGING_LEVEL}])
